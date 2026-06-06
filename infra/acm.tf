afe427dd7651bbd55cc6f455ce2f3aa23db98b87
Mauri <temaurice2000@yahoo.fr>
2026-06-06 15:43:50 -0400

    feat(infra): ACM cert with DNS validation

---
+++ infra/acm.tf
@@ -0,0 +1,32 @@
+# Certificate for bekofc.com + www.bekofc.com, in us-east-1 (CloudFront requirement).
+resource "aws_acm_certificate" "site" {
+  provider          = aws.us_east_1
+  domain_name       = var.domain_name
+  subject_alternative_names = ["www.${var.domain_name}"]
+  validation_method = "DNS"
+
+  lifecycle { create_before_destroy = true }
+}
+
+# DNS validation records, written to the Route 53 zone.
+resource "aws_route53_record" "cert_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.site.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  zone_id = aws_route53_zone.primary.zone_id
+  name    = each.value.name
+  records = [each.value.record]
+  ttl     = 60
+  type    = each.value.type
+}
+
+resource "aws_acm_certificate_validation" "site" {
+  provider                = aws.us_east_1
+  certificate_arn         = aws_acm_certificate.site.arn
+  validation_record_fqdns = [for r in aws_route53_record.cert_validation : r.fqdn]
+}
--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,2 +1,5 @@
 output "site_bucket_name"  { value = aws_s3_bucket.site.bucket }
 output "media_bucket_name" { value = aws_s3_bucket.media.bucket }
+
+output "route53_zone_name_servers" { value = aws_route53_zone.primary.name_servers }
+output "acm_certificate_arn"       { value = aws_acm_certificate_validation.site.certificate_arn }
